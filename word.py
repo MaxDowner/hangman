e6a29616b1a16ca35c0666fd8fd6255f9a30d9fa
Salona Mallick <salona@MacBook-Air.local>
2025-08-29 14:09:19 +0100

    Add file word.py

--- word.py
+++ word.py
@@ -9,3 +9,5 @@ def guess(): # takes a guess from user and stores it in already_guessed list
     guessed_letter = input('What is your guess?')
     already_guessed.append(guessed_letter)
     return already_guessed
+
+word_input()
